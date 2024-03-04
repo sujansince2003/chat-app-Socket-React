41df392ce950969efdbd9ff76212ecdde737c699
sujan khatri <sujansince2059@gmail.com>
2024-03-04 22:27:37 +0545

    socket server with node and express

--- server/index.js
+++ server/index.js
@@ -2,7 +2,26 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
 
 const server = http.createServer(app);
 
-const io
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected : ${socket.id}`);
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server is running");
+});
