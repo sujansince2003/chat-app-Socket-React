e82186406cc52acff8a5692a4ab2f219036627e7
sujan khatri <sujansince2059@gmail.com>
2024-03-04 22:26:49 +0545

    App component for connectiing and rendering UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
 
+import "./App.css";
+
+const socket = io.connect("localhost:3001");
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  function sendMessage() {
+    const newMessage = { text: message, type: "sent" };
+    socket.emit("send_message", newMessage);
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (msg) => {
+      const newMessage = { text: msg.text, type: "received" };
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    });
+  }, [socket]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="chat-box">
+      <div className="messages-container">
+        {messages.map((msg, index) => (
+          <div className={msg.type} key={index}>
+            <p>{msg.text}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="send">
+        <input onChange={(e) => setMessage(e.target.value)} type="text" />
+        <button onClick={sendMessage}>Send</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
